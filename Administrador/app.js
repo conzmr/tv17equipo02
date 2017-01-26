ae3605a71749b4fbce582a7b95573394721470f8
vic3r <barcelonamane@hotmail.com>
2017-01-26 06:17:01 -0600

    Archivos

--- Administrador/app.js
+++ Administrador/app.js
@@ -1,35 +1,28 @@
-// Initialize Firebase
+		  // Initialize Firebase
 (function() {
-    var config = {
-        apiKey: "AIzaSyCGtYN3lyGjmAOwTdFujyLouz0Cunlb4s4",
-        authDomain: "puertadehierro-a797c.firebaseapp.com",
-        databaseURL: "https://puertadehierro-a797c.firebaseio.com",
-        storageBucket: "puertadehierro-a797c.appspot.com",
-        messagingSenderId: "442464985582"
-    };
-    firebase.initializeApp(config);
-    const uList = document.getElementById('list'); //Referencia a lista 
-    var big = document.getElementById('big');
+		  var config = {
+		    apiKey: "AIzaSyCGtYN3lyGjmAOwTdFujyLouz0Cunlb4s4",
+		    authDomain: "puertadehierro-a797c.firebaseapp.com",
+		    databaseURL: "https://puertadehierro-a797c.firebaseio.com",
+		    storageBucket: "puertadehierro-a797c.appspot.com",
+		    messagingSenderId: "442464985582"
+		  };
+		  firebase.initializeApp(config);
+		  const uList = document.getElementById('list');
+		  var big = document.getElementById('big');
 
-    var dbRef = firebase.database().ref().child('text');
-    var dbRefList = firebase.database().ref().child('Usuarios/mta1'); // Referencia al hijo Contacto
+		  var dbRef = firebase.database().ref().child('text');
+		  var dbRefList = firebase.child('Contacto');
 
-    dbRef.on('value', snap => big.innerText = snap.val());
-    var firebaseRef = firebase.database().ref();
-    firebaseRef.child('users').on('value', function(groupSnap) {
-        groupSnap.forEach(function(snap) {
-            //console.log(snap.val());
-            var object = snap.val();
-            Object.keys(object).forEach(function(key, index) {
-                const li = document.createElement('li');
-                li.innerText = (key + ": " + object[key]);
-                //li.id = snap.key();
-                uList.appendChild(li);
-            });
-            const li = document.createElement('li');
-            li.innerText = snap.val();
-            uList.appendChild(li);
-        });
-    });
+		  dbRef.on('value', snap => big.innerText = snap.val());
+		  /*dbRef.on('value', snap => {
+		  	big.innerText = JSON.stringify(snap.val(), null, 3);
+		  }*/
+		  dbRefList.on('child_added', snap => {
+		  	const li = document.createElement('li');
+		  	li.innerText = snap.val();
+		  	uList.appendChild(li);
+		  }
 
+		  //const preObject = document.getElementById('object');
 }());
